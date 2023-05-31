00480b4dd2db7a44c2fe78559b19bfa7fa2a36c7
Irem Nur Gozudok <iremgozudok99@icloud.com>
2023-05-31 16:07:37 +0300

    projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AboutMe from "./components/AboutMe";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <AboutMe />
       <Skills />
+      <Projects />
     </div>
   );
 }
---
+++ src/data/data.jsx
@@ -0,0 +1,115 @@
+import Blooming from "../asset/blooming.png";
+import Freuze from "../asset/freuze.png";
+import MomHous from "../asset/momHaus.png";
+import MultiEmlak from "../asset/multiemlak.png";
+import Prusa from "../asset/Prusa.png";
+import FakeTweet from "../asset/fakeTweet.png";
+import BudgetReact from "../asset/budgetApp.png";
+import BudgetJS from "../asset/js-budget.png";
+import TodoJS from "../asset/js-todo.png";
+import Pomodoro from "../asset/pomodoro.png";
+import QuizApp from "../asset/quiz.png";
+import RecipeApp from "../asset/recipe.png";
+import ShoppingApp from "../asset/shopping.png";
+import TodoReact from "../asset/todoApp.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "HTML/CSS",
+    image: Blooming,
+    link: "https://bloomingbrothers.com/",
+    path: "Visit Website",
+  },
+  {
+    id: 2,
+    name: "HTML/CSS",
+    image: Freuze,
+    link: "https://www.freuzecoffee.com/",
+    path: "Visit Website",
+  },
+  {
+    id: 3,
+    name: "HTML/CSS",
+    image: MomHous,
+    link: "https://momhaus.com/",
+    path: "Visit Website",
+  },
+  {
+    id: 4,
+    name: "HTML/CSS",
+    image: MultiEmlak,
+    link: "https://www.multiemlak.com/",
+    path: "Visit Website",
+  },
+  {
+    id: 4,
+    name: "HTML/CSS",
+    image: Prusa,
+    link: "https://www.prusaprekast.com/",
+    path: "Visit Website",
+  },
+  {
+    id: 6,
+    name: "React JS Application",
+    image: FakeTweet,
+    link: "https://github.com/iremgozudok/fake-tweet",
+    path: "Code",
+  },
+  {
+    id: 7,
+    name: "React JS Application",
+    image: BudgetReact,
+    link: "https://github.com/iremgozudok/budget-app",
+    path: "Code",
+  },
+  {
+    id: 8,
+    name: "JavaScript Application",
+    image: BudgetJS,
+    link: "https://github.com/iremgozudok/track-budget",
+    path: "Code",
+  },
+  {
+    id: 9,
+    name: "JavaScript Application",
+    image: TodoJS,
+    link: "https://github.com/iremgozudok/todo-app",
+    path: "Code",
+  },
+  {
+    id: 10,
+    name: "React JS Application",
+    image: Pomodoro,
+    link: "https://github.com/iremgozudok/pomodoro-app",
+    path: "Code",
+  },
+  {
+    id: 11,
+    name: "JavaScript Application",
+    image: QuizApp,
+    link: "https://github.com/iremgozudok/quiz-app",
+    path: "Code",
+  },
+  {
+    id: 12,
+    name: "JavaScript Application",
+    image: RecipeApp,
+    link: "https://github.com/iremgozudok/meal-app",
+    path: "Code",
+  },
+  {
+    id: 13,
+    name: "JavaScript Application",
+    image: ShoppingApp,
+    link: "https://github.com/iremgozudok/shopping-app",
+    path: "Code",
+  },
+  {
+    id: 14,
+    name: "React JS Application",
+    image: TodoReact,
+    link: "https://github.com/iremgozudok/todo-react",
+    path: "Code",
+  },
+];
