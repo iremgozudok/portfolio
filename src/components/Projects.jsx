7ce701d0c2868de04547b21abfff2f520f8b39e9
Irem Nur Gozudok <iremgozudok99@icloud.com>
2023-06-01 11:45:36 +0300

    projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import { data } from "../data/data";
 
 function Projects() {
   const project = data;
-  //Comment
+  //Comment var
   return (
     <div name="projects" className="w-full">
       <div className="max-w-[1000px] mx-auto py-40 px-4 flex flex-col justify-center w-full h-full">
