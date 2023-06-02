e16bce19bee5f8c6f4adf5ff23d830aebf87d98c
Irem Nur Gozudok <iremgozudok99@icloud.com>
2023-06-02 20:47:32 +0300

    final edits

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import {
+  AiOutlineArrowRight,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+import { IoIosMail } from "react-icons/io";
 
 function AboutMe() {
   return (
-    <div name="about-me" className="w-full h-screen">
+    <div id="about-me" className="w-full h-screen">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-500">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-gray-700">
@@ -13,13 +18,32 @@ function AboutMe() {
           I'm a Front-end Developer.
         </h2>
         <p className="text-gray-500 max-w-[700px] py-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          beatae nesciunt earum! Vel tenetur molestiae sequi, blanditiis ipsam
-          expedita harum?
+          I’m passionate about software development and I’m skilled in HTML,
+          CSS, JavaScript, and React. I’m now looking for a junior dev position
+          as I recently learned how to code by myself.
         </p>
         <div>
-          <button className="border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-500 hover:text-white">
-            View Projects <AiOutlineArrowRight className="ml-2" />
+          <button className="py-2 my-2 flex items-center">
+            <div className="flex justify-center w-full gap-4">
+              <div className="bg-gray-100 hover:bg-pink-100 rounded-full p-3">
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/irem-nur-gozudok-208b11260/"
+                >
+                  <AiFillLinkedin size={32} />
+                </a>
+              </div>
+              <div className="bg-gray-100 hover:bg-pink-100 rounded-full p-3">
+                <a target="_blank" href="https://github.com/iremgozudok">
+                  <AiFillGithub size={32} />
+                </a>
+              </div>
+              <div className="bg-gray-100 hover:bg-pink-100 rounded-full p-3">
+                <a target="_blank" href="mailto:iremgozudok99@icloud.com">
+                  <IoIosMail size={32} />
+                </a>
+              </div>
+            </div>
           </button>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,15 +8,20 @@ export default function Navbar() {
   const handleClick = () => setNav(!nav);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 text-gray-700 bg-white">
-      <div>
-        <img src={Logo} alt="logo image" className="w-28" />
+      <div className="z-10">
+        <img src={Logo} alt="logo image" className="w-20" />
       </div>
 
       <ul className="hidden md:flex text-xl">
-        <li className="p-10 cursor-pointer">About Me</li>
-        <li className="p-10 cursor-pointer">Skills</li>
-        <li className="p-10 cursor-pointer">Projects</li>
-        <li className="p-10 cursor-pointer">Contact</li>
+        <li className="p-10 cursor-pointer">
+          <a href="#about-me">About Me</a>
+        </li>
+        <li className="p-10 cursor-pointer">
+          <a href="#skills">Skills</a>
+        </li>
+        <li className="p-10 cursor-pointer">
+          <a href="#projects">Projects</a>
+        </li>
       </ul>
 
       <div onClick={handleClick} className="md:hidden z-10">
@@ -30,10 +35,21 @@ export default function Navbar() {
             : "absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center bg-white"
         }
       >
-        <li className="p-10 cursor-pointer">About Me</li>
-        <li className="p-10 cursor-pointer">Skills</li>
-        <li className="p-10 cursor-pointer">Projects</li>
-        <li className="p-10 cursor-pointer">Contact</li>
+        <li className="p-5 font-bold text-2xl cursor-pointer">
+          <a onClick={handleClick} href="#about-me">
+            About Me
+          </a>
+        </li>
+        <li className="p-5 font-bold text-2xl cursor-pointer">
+          <a onClick={handleClick} href="#skills">
+            Skills
+          </a>
+        </li>
+        <li className="p-5 font-bold text-2xl cursor-pointer">
+          <a onClick={handleClick} href="#projects">
+            Projects
+          </a>
+        </li>
       </ul>
     </div>
   );
